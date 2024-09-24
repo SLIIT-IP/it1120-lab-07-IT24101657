4198612de3495b7c72b3cfbe1f1d1b45c8cf9dd8
IT24101657 <it24101657@my.sliit.lk>
2024-09-24 15:36:34 +0530

    IT24101657Lab7Q1.java

---
+++ Q1/IT24101657Lab7Q1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class IT24101657Lab7Q1
+{
+	public static void main (String[]args)
+	{
+	
+		Scanner input = new Scanner (System.in);
+		int mark1,mark2,mark3,mark4;
+
+		double average;
+		
+		System.out.print("Enter mark for four subjects: ");
+		
+		System.out.print("Enter subject mark 1 :");
+		mark1 = input.nextInt();
+		System.out.print("Enter subject mark 2 :");
+		mark2 = input.nextInt();
+		System.out.print("Enter subject mark 3 :");
+		mark3 = input.nextInt();
+		System.out.print("Enter subject mark 4 :");
+		mark4 = input.nextInt();
+
+		average = (mark1+mark2+mark3+mark4)/4.0;
+
+		System.out.println("Average is: "+ average);
+
+		
+ 		if(average>=75 && average<=100)
+			{
+			System.out.println("Grade: Distinction");
+			}
+
+			else if(average>=50&& average<=74){
+				System.out.println("Grade: Credit");
+			}
+				else if(average>=0 && average<=49){
+					System.out.println("Grade: Fail");
+			}
+					else{
+						System.out.println("Invalid Marks");}
+
+	}
+}
